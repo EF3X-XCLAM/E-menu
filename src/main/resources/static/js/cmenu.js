7795d3f0821614f2da64f911bab9ae0d2cfac9ab
Leo-Muo <v.muoghara@gmail.com>
2021-03-23 21:30:08 +0200

    - Quantity of chosen items total price is reflected on "total tab" -Add getTotal function to the javascript file

--- src/main/resources/static/js/cmenu.js
+++ src/main/resources/static/js/cmenu.js
@@ -1,13 +1,15 @@
 var incBtn = document.getElementsByClassName('inc');
 var decBtn = document.getElementsByClassName('dec');
+var total = parseFloat(document.getElementById('tot').innerHTML);
 	//console.log(incBtn);
 	//console.log(decBtn);
-	
+
 	///Increment
 	for(var i = 0; i < incBtn.length; i++){
 		var btn = incBtn[i];
+
 		btn.addEventListener('click', function(event){
-			
+
 			var btnClicked = event.target;
 			//console.log(btnClicked);
 			var input = btnClicked.parentElement.children[1];
@@ -17,17 +19,18 @@ var decBtn = document.getElementsByClassName('dec');
 			if(inputValue < parseInt(input.max)){
 				input.value = parseInt(inputValue)+ 1;
 				//console.log(input.value);
+				getTotal(btnClicked.parentElement.parentElement.children[1], "inc");
 			}
-			//input.value = parseInt(inputValue)+ 1;
 			//console.log(input.value);
+
 		})
 	}
 
-	///Decrement
 	for(var i = 0; i < decBtn.length; i++){
 		var btn = decBtn[i];
+
 		btn.addEventListener('click', function(event){
-			
+
 			var btnClicked = event.target;
 			//console.log(btnClicked);
 			var input = btnClicked.parentElement.children[1];
@@ -37,8 +40,35 @@ var decBtn = document.getElementsByClassName('dec');
 			if(inputValue >= parseInt(input.min)){
 				input.value = parseInt(inputValue)-1;
 				//console.log(input.value);
+				getTotal(btnClicked.parentElement.parentElement.children[1], "dec");
 			}
-			//input.value = parseInt(inputValue)+ 1;
 			//console.log(input.value);
+
 		})
+	}
+
+	function getTotal(pPrice,ope){
+
+		//	console.log(ope);
+
+
+		var price = pPrice.childNodes[1];;
+		console.log(price.innerHTML);
+
+		if(ope == 'inc'){
+			//console.log("inc");
+			//console.log(total);
+			total = parseFloat(price.innerHTML) + total;
+		}else if(total > 0){
+			//console.log("dec");
+			//console.log(total);
+			total = total - parseFloat(price.innerHTML) ;
+		}
+		//console.log(total);
+		if(total < 0){
+			document.getElementById('tot').innerHTML = 0;
+		}else{
+			document.getElementById('tot').innerHTML = total.toFixed(2);
+		}
+
 	}
