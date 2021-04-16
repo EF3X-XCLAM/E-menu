ec88e0e9456baae5b8c0e9c9120ebad70b36d6d2
Leo-Muo <v.muoghara@gmail.com>
2021-04-16 18:52:31 +0300

    "-> Connect the program to the database -> Add the category class that maps to the category table in the database "

---
+++ src/main/java/com/emenu/emenu/Category.java
@@ -0,0 +1,52 @@
+package com.emenu.emenu;
+
+
+import javax.persistence.*;
+
+
+@Entity(name = "Category")
+@Table(name = "category")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    @Column(name="ctID")
+    private Long ctID;
+
+    @Column(
+            name="ct_name",
+            nullable = false,
+            columnDefinition = "TEXT",
+            unique = true
+    )
+    private String ct_name;
+
+    public Category(Long ctID, String ct_name) {
+        this.ctID = ctID;
+        this.ct_name = ct_name;
+    }
+
+    public Long getCtID() {
+        return ctID;
+    }
+
+    public void setCtID(Long ctID) {
+        this.ctID = ctID;
+    }
+
+    public String getCt_name() {
+        return ct_name;
+    }
+
+    public void setCt_name(String ct_name) {
+        this.ct_name = ct_name;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "ctID=" + ctID +
+                ", ct_name='" + ct_name + '\'' +
+                '}';
+    }
+}
